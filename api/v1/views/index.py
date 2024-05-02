0f8cfcb6bd51dd424d5086c257f286aecb21fa6a
felabel <felicityabel2016@gmail.com>
2024-05-02 19:53:57 +0100

    executables

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,45 @@
 #!/usr/bin/python3
 """
-index
+app
 """
 
-from flask import jsonify
-from api.v1.views import app_views
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
 
+from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
-def status():
+app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
     """
-    status route
-    :return: response with json
+    teardown function
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
     """
     data = {
-        "status": "OK"
+        "error": "Not found"
     }
 
     resp = jsonify(data)
-    resp.status_code = 200
+    resp.status_code = 404
 
-    return resp
+    return(resp)
 
-
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
-def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
