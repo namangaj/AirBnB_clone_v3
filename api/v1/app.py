9528327d3e1ab05c009498dd385b65b66a4810c8
felabel <felicityabel2016@gmail.com>
2024-05-02 20:05:04 +0100

    task correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,20 +26,5 @@ def teardown(exception):
     storage.close()
 
 
-@app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
-
 if __name__ == "__main__":
     app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
