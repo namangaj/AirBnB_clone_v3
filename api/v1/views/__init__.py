62eefc6b0a7b306c1a91be31458d98c0a0965193
Joshua Namanga <namangaj@gmail.com>
2024-05-03 03:01:42 -0700

    updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""Init file for views module"""
-from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+"""
+createw Flask app blueprint.
+"""
+from api.v1.index import *
+from flask import blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', _name_, url_prefix='/api/vi')
